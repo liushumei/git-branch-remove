9304bc4517a632ddbd8138b8991ad7ee062977e6
liushumei <liushumei@mokahr.com>
2023-01-08 10:23:32 +0800

    同时删除远程分支

--- bin/index.js
+++ bin/index.js
@@ -1,33 +1,48 @@
 #!/usr/bin/env node
-import { execaCommand, execaCommandSync } from 'execa'
+import { execaCommand } from 'execa'
 import inquirer from 'inquirer'
 
-function getGitBranch() {
-  const res = execaCommandSync('git status');
-  console.info('res----', res)
-  return res.stdout;
-}
-const branches = getGitBranch()
-// console.info('branched', branches)
-// inquirer
-//   .prompt([
-//     {
-//       type: 'checkbox',
-//       message: '请选择要删除的分支',
-//       name: 'branches',
-//       default: '',
-//       choices: ['branch1', 'jkac', 'haha'],
-//     },
-//     {
-//       type: 'confirm',
-//       message: '是否同时删除远程分支?',
-//       name: 'isRemoveOrigin',
-//       default: false,
-//     },
-//   ])
-//   .then((answers) => {
-//     console.log('answers', answers);
-//   })
-//   .catch((error) => {
-//     console.log('error', error);
-//   });
+execaCommand('git branch')
+ .then((res = '') => {
+  const branchStr = res.stdout || ''
+  const choices = branchStr.trim().split('\n')
+    .filter((branch) => {
+      return branch && !['master', 'gray-release', 'release'].includes(branch.trim()) && !branch.startsWith('*')
+    })
+    .map((branch) => branch.trim());
+  if (!choices.length) {
+    console.log('暂无可删除的本地分支')
+    return
+  }
+  inquirer
+  .prompt([
+    {
+      type: 'checkbox',
+      message: '请选择要删除的分支（已自动过滤master、gray-release、release分支）',
+      name: 'removeLocal',
+      default: '',
+      choices,
+    },
+    {
+      type: 'confirm',
+      message: '是否同时删除远程分支?',
+      name: 'removeOrigin',
+      default: false,
+    },
+  ])
+  .then((answers) => {
+    const branchStr = answers.removeLocal.join(' ');
+    execaCommand('git branch -d ' + (answers.removeOrigin ? '-r ' : '') + branchStr)
+      .then(() => {  
+        console.log('已成功删除分支：' + branchStr)
+      })
+      .catch((err) => {
+        console.log('删除分支失败' + err)
+      })
+  })
+  .catch((error) => {
+    console.log('error', error);
+  });
+}).catch((err) => {
+  console.info('获取本地分支失败：', err)
+});
